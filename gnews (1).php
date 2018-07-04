9fdc63c4edeef790893163f8feaf8d51c21f6d70
dpoda <40798686+dpoda@users.noreply.github.com>
2018-07-04 17:15:59 +0200

    Revert "фвв1"
    This reverts commit 8beba5fe39c9307732b1cdaaf2647e4670ad2568.

---
+++ gnews (1).php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>gNEWS</title>
+
+<link href="https://fonts.googleapis.com/css?family=Sancreek" rel="stylesheet">
+    
+
+
+<link rel="shortcut icon" href="logo.jpg" />
+
+<style>
+#menu a{text-decoration:none;
+    color:black;
+    
+}
+#menu ul{list-style:none;
+    font-size:22px;
+    position:absolute;
+    
+}
+.logo {
+    padding:0px;
+}
+.logo p {display:inline-block;
+font-family: 'Gothical';
+
+justify-content:center;
+display:flex;
+font-size:45px;
+margin:0px;
+}
+ .hr1{
+    border:none;
+    color:black;
+background-color:black;
+    height:1px;
+
+}
+.hr2{border:none;
+    color:black;
+background-color:black;
+    height:2px;}
+    
+.soc{
+    float:right;}
+    .content{float:right;}
+
+ img {
+    height:270px;
+    width:auto;
+    float:left;
+    margin-right:20px;
+    
+}
+.soc > img {width:20px;
+    height:auto;
+    float:right;
+}
+.vk > img {width:22px;
+    height:auto;
+    float:right;}
+    
+    .title{font-size:22px;
+        
+    }
+   .akapit { 
+       text-indent:2em;
+       margin-top:-10px;
+   }
+.footer{clear:both;}
+.top{float:left;
+    width:30%;
+    margin-right:2%;
+}
+.content{float:right;
+    width:67%;
+}
+
+</style>
+</head>
+<body>
+
+<ul id="menu">
+    <li>
+        <a href="#"><img src='menu.png' style="width:40px;height:auto; margin-left:-45px;"></a>
+        <ul>
+            <li><a href="#">News</a></li>
+            <li><a href="#">Home Page</a></li>
+            <li><a href="#">World</a></li>
+            <li><a href="#">Politics</a></li>
+            <li><a href="#">Bussines</a></li>
+            <li><a href="#">EU</a></li>
+            <li><a href="#">Arts</a></li>
+            <li><a href="#">Today's Arts</a></li>
+            <li><a href="#">Art&Design</a></li>
+            <li><a href="#">Books</a></li>
+            <li><a href="#">Movies</a></li>
+            <li><a href="#">Living</a></li>
+            <li><a href="#">Automobiles</a></li>
+            <li><a href="#">CrossWord</a></li>
+            <li><a href="#">Food</a></a></li>
+            <li><a href="#">Education</a></li>
+            <li><a href="#">Fashion&Style</a></li>
+            <li><a href="#">Health</a></li>
+            
+            
+            
+        </ul>
+    </li>
+   
+</ul>
+    
+<div class="logo">
+    
+<p>gNEWS<p>
+   
+</div>
+    <div class='soc'>
+     <div class='vk'>   <img src="vk1.jpg">  </div> 
+       <div class='soc'> <img src="inst.png"> </div> 
+        <div class='soc'><img src="tel.png">  </div> 
+        <div class='soc'><img src="twitter.jpg"> </div> 
+        <div class='soc'><img src="you.jpg"> </div> 
+        
+   
+    <br>
+<hr class="hr1">
+<script>
+Data = new Date();
+Year = Data.getFullYear();
+Month = Data.getMonth();
+Day = Data.getDate();
+switch(Month){
+case 0: fMonth="January";break;
+case 1:fMonth="February";break;
+case 2:fMonth="March";break;
+case 3:fMonth="April";break;
+case 4:fMonth="May";break;
+case 5:fMonth="June";break;
+case 6:fMonth="July";break;
+case 7:fMonth="August";break;
+case 8:fMonth="September";break;
+case 9:fMonth="October";break;
+case 10:fMonth="November";break;
+case 11:fMonth="December";break;
+    }
+document.write(fMonth+" "+Day+" , "+Year);
+</script>
+<hr class='hr2'>
+<p style="font-size:22px;"><b>TOP NEWS</b></p>
+<div class='top'> <img src="sylwa.jpg">
+<p style="font-size:13px"><i>Source: Letteralmente Felice!</i></p></div>
+<div class='content'>
+<div class='title'><b>Sylvia Plath's Pulitzer Prize in Poetry Is Up for Auction</b></div>
+<br>
+<p class='akapit'>
+A Pulitzer Prize in Poetry that was awarded posthumously to Sylvia Plath in 1982 for her book The Collected Poems will be auctioned on June 28. The Los Angeles-based Nate D. Sanders Auctions house says bidding for the literary document will start at $40,000. </p><p class='akapit'>
+The complete book of Plath’s poetry was published in 1981—18 years after her death—and was edited by her husband, fellow poet Ted Hughes. The Pulitzer Prize was presented to Hughes on Plath’s behalf, and one of two telegrams sent by Pulitzer President Michael Sovern to Hughes read, “We’ve just heard that the Collected Plath has won the Pulitzer Prize. Congratulations to you for making it possible.” The telegrams will also be included in the lot, in addition to an official congratulatory letter from Sovern. </p><p class='akapit'>
+The Pultizer’s jury report from 1982 called The Collected Poems an “extraordinary literary event.” It went on to write, “Plath won no major prizes in her lifetime, and most of her work has been posthumously published … The combination of metaphorical brilliance with an effortless formal structure makes this a striking volume.” 
+Ted Hughes penned an introduction to the poetry collection describing how Plath had “never scrapped any of her poetic efforts,” even if they weren’t all masterpieces. He wrote: 
+“Her attitude to her verse was artisan-like: if she couldn’t get a table out of the material, she was quite happy to get a chair, or even a toy. The end product for her was not so much a successful poem, as something that had temporarily exhausted her ingenuity. So this book contains not merely what verse she saved, but—after 1956—all she wrote.”  </p><p class='akapit'>
+Also up for auction is Plath’s Massachusetts driver’s license from 1958, at which time she went by the name Sylvia P. Hughes. Bidding for the license will begin at $8000.</p>
+</div>
+
+<div class='footer'>
+<hr class="hr2">
+<div>Contact Us </div>
+</div>
+</body>
+
+</html>
